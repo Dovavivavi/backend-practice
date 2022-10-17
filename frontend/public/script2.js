2fccdbc8265caf15f08231f1e2e0e44167e0f940
Dovavivavi <szilagyiabel1@gmail.com>
2022-10-17 21:11:35 +0200

    arr methods

--- frontend/public/script2.js
+++ frontend/public/script2.js
@@ -67,4 +67,57 @@ window.addEventListener('load', () => {
     //   });
     // });
   });
+
+   //ARRAY METHODS:
+   const items = [
+    {name: 'Bike', price: 200},
+    {name: 'Game', price: 500},
+    {name: 'Soap', price: 800},
+    {name: 'Book', price: 60},
+    {name: 'Phone', price: 800},
+    {name: 'CD', price: 900},
+    {name: 'Console', price: 273},
+    {name: 'TV', price: 221},
+    {name: 'Shoe', price: 189},
+    {name: 'T-shirt', price: 145},
+    {name: 'Trousers', price: 150}
+  ];
+  console.log(items)
+
+  //filter:
+  const filteredArr = items.filter((item) => {
+    return item.price >= 800
+  });
+  console.log(filteredArr);
+
+  //map:
+  const mappedArr = items.map((item) => {
+    return item.name
+  });
+  console.log(mappedArr);
+
+  //find:
+  const foundItem = items.find((item) => {
+    return item.name === 'CD'
+  });
+  console.log(foundItem)
+
+  //forEach:
+  items.forEach((i) => {
+    console.log(i.name)
+  });
+
+  //some (checks if there are any matched items):
+  const hasFreeItem = items.some((it) => {
+    return it.price = 0
+  });
+  console.log(hasFreeItem);
+
+  //every (checks if every item matches the param):
+  const isEveryBelow1k = items.every((item) => {
+    return item.price < 1000
+  })
+  console.log(isEveryBelow1k)
+
+  
 });
